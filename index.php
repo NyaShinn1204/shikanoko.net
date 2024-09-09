9fca9e7590c20b2171dae8fc0e527bc13a999eb2
NyaShinn1204 <team.nyarara@gmail.com>
2024-09-09 08:31:56 +0000

    Zipped png to jpg
    - 圧縮

--- index.php
+++ index.php
@@ -69,7 +69,7 @@ function generateRandomString($length = 10)
     }
 
     body {
-        background-image: url("https://shikanoko.net/assets/image/shika_background.png");
+        background-image: url("https://shikanoko.net/assets/image/shika_background-zipped.jpg");
         background-attachment: fixed;
         background-size: cover;
         background-position: center center;
